9d15478bfaffde717d37b94c572088799da8d06f
Your Name <aparnitstudy@gmail.com>
2025-07-14 23:51:15 +0530

    setting up react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router";
+
 import './index.css'
 import Home from './pages/Home'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/authentication/login",
+        element: <div>Login</div>
+    }
+]);
+
 createRoot(document.getElementById('root')).render(
-    <Home />
+    <RouterProvider router={router} />
 )
