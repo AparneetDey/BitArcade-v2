96d5eebf25ba3d153c24b3a5ca06cebf977c5ccb
Your Name <aparnitstudy@gmail.com>
2025-07-19 17:41:54 +0530

    working on profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 
-const Profile = ({searchTerm, setSearchTerm, userData}) => {
+const Profile = ({ searchTerm, setSearchTerm, userData }) => {
   return (
-    <div>
+    <main>
       <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} userData={userData} searchBar={false} />
-    </div>
+
+      <section className=' px-4 md:px-10'>
+        <div className='flex justify-center items-center md:justify-start md:items-start '>
+          <div className='flex flex-col gap-4 justify-center items-center'>
+            <div className='profile-pic'>
+              <img src="/profile-icon.png" alt="Profile Icon" />
+            </div>
+            <h2 className='flex gap-2'>{userData.username}<img className='cursor-pointer' src="/edit-pencil.svg" alt="" /></h2>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
