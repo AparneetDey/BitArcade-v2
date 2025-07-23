a8e9315552be6996c524962998288f29381fbf1f
Your Name <aparnitstudy@gmail.com>
2025-07-23 22:26:57 +0530

    CORS fixing

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,12 @@ app.use(cors({
 	credentials: true
 }));
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
+	next();
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
